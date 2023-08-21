af1321db4c646fde4780125bba838dc99db9f2f6
Lewis Crichton <lewi@lewisakura.moe>
2023-08-21 15:19:33 +0100

    chore: remove useless constants

--- installer/constants.go
+++ installer/constants.go
@@ -6,8 +6,6 @@
 
 package installer
 
-import "image/color"
-
 // these are replaced by the linker
 
 var InstallerGitHash = "Unknown"
@@ -19,13 +17,6 @@ const InstallerReleaseUrl = "https://api.github.com/repos/Vencord/Installer/rele
 const InstallerReleaseUrlFallback = "https://vencord.dev/releases/installer"
 
 var UserAgent = "VencordInstaller/" + InstallerGitHash + " (https://github.com/Vencord/Installer)"
-
-var (
-	DiscordGreen  = color.RGBA{R: 0x2D, G: 0x7C, B: 0x46, A: 0xFF}
-	DiscordRed    = color.RGBA{R: 0xEC, G: 0x41, B: 0x44, A: 0xFF}
-	DiscordBlue   = color.RGBA{R: 0x58, G: 0x65, B: 0xF2, A: 0xFF}
-	DiscordYellow = color.RGBA{R: 0xfe, G: 0xe7, B: 0x5c, A: 0xff}
-)
 
 var LinuxDiscordNames = []string{
 	"Discord",
